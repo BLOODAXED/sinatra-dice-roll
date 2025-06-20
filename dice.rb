d5883d05082a98cccee2c5909b703543edc51c35
A <bloodaxed@gmail.com>
2025-06-20 00:31:43 +0000

    added single view for all rolls

--- dice.rb
+++ dice.rb
@@ -13,36 +13,36 @@ get("/") do
 end
 
 get("/dice/2/6") do
+  @roll = "2d6"
   first_die = rand(1..6)
   second_die = rand(1..6)
   sum = first_die + second_die
 	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+  erb(:dice_roll)
 end
 
 get("/dice/2/10") do
+  @roll = "2d10"
   first_die = rand(1..10)
   second_die = rand(1..10)
   sum = first_die + second_die
 	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+  erb(:dice_roll)
 end
 
 get("/dice/1/20") do
+	@roll = "1d20"
+  @outcome = "You rolled a #{rand(1...20)}."
 	
-  outcome = "You rolled a #{rand(1...20)}."
-	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+  erb(:dice_roll)
 end
 
 get("/dice/5/4") do
+  @roll = "5d4"
   first_die = rand(1..4)
   second_die = rand(1..6)
   third_die = rand(1..4)
@@ -50,8 +50,7 @@ get("/dice/5/4") do
   fifth_die = rand(1..4)
   sum = first_die + second_die + third_die + fourth_die + fifth_die
 	
-  outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and a #{fifth_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and a #{fifth_die} for a total of #{sum}."
 	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+  erb(:dice_roll)
 end
